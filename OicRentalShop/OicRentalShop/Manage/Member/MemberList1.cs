a2cc7e3a92f690c5d226519271fc7c34d6284558
unknown <B6080@oic.osaka.jp>
2017-11-10 13:25:32 +0900

    会員管理、従業員管理、商品管理の画面作成

--- OicRentalShop/OicRentalShop/Manage/Member/MemberList1.cs
+++ OicRentalShop/OicRentalShop/Manage/Member/MemberList1.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Drawing;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace OicRentalShop.Manage.Staff
+namespace OicRentalShop.Manage.Member
 {
-    public partial class StaffList : UserControl
+    public partial class MemberList1 : Form
     {
-        public StaffList()
+        public MemberList1()
         {
             InitializeComponent();
         }
