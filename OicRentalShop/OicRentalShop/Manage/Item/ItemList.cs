d2f273f8276f1a5c1103f9af0b462ead1dd34292
agoetc <b6151@oic.jp>
2017-11-24 16:10:20 +0900

    一時commit

--- OicRentalShop/OicRentalShop/Manage/Item/ItemList.cs
+++ OicRentalShop/OicRentalShop/Manage/Item/ItemList.cs
@@ -40,7 +40,8 @@ namespace OicRentalShop.Manage.Item
 
             if (cmb_Type.Text == "DVD")
             {
-                selectfunc("SELECT dt.TITLE_NAME AS タイトル名,dt.TITLE_ID AS タイトルID,di.ITEM_ID AS 商品ID,ty.TYPE_NAME AS 商品タイプ,dg.GENRE_NAME AS ジャンル FROM TBL_TITLE dt,TBL_ITEM di,TBL_TYPE ty,TBL_GENRE dg WHERE dt.TYPE_ID=ty.TYPE_ID AND di.TITLE_ID=dt.TITLE_ID AND dt.GENRE_ID = dg.GENRE_ID AND dt.TYPE_ID=1");
+
+                selectfunc("SELECT title.TITLE_NAME AS タイトル名, title.TITLE_ID AS タイトルID , item.ITEM_ID AS 商品ID , type.TYPE_NAME AS 商品タイプ , genre.GENRE_NAME AS ジャンル FROM TBL_TITLE title , TBL_ITEM item , TBL_TYPE type , TBL_GENRE genre WHERE type.TYPE_ID = type.TYPE_ID AND item.TITLE_ID = title.TITLE_ID AND title.GENRE_ID = genre.GENRE_ID AND dt.TYPE_ID=1");
             }
             else if (cmb_Type.Text == "CD")
             {
@@ -69,14 +70,14 @@ namespace OicRentalShop.Manage.Item
         private void genreLoad()
         {
             //CD・DVDのどちらかが選択されるとジャンルを流し込む
-            String sql = "SELECT GENRE_NAME FROM TBL_GENRE WHERE TYPE_ID = ";
+            String sql = "SELECT GENRE_NAME FROM TBL_GENRE WHERE TYPE_ID =";
             if (cmb_Type.Text == "DVD")
             {
-                sql += "1";
+                sql += " 1";
             }
             else if (cmb_Type.Text == "CD")
             {
-                sql += "2";
+                sql += " 2";
             }
             else
             {
@@ -99,6 +100,24 @@ namespace OicRentalShop.Manage.Item
             oleCmd.Dispose();
         }
 
-        private void serch() { }
+        private String serchItems()
+        {
+
+            String serchSql = "";
+
+            if (txt_TitleName.Text != "")
+            {
+                serchSql += " AND title.TITLE_NAME = " + txt_TitleName.Text; 
+            }
+            if (txt_TitleID.Text != "")
+            {
+                serchSql += " AND title.TITLE_ID =" + txt_TitleID.Text;
+            }
+
+
+
+            return serchSql;
+        }
     }
 }
+
