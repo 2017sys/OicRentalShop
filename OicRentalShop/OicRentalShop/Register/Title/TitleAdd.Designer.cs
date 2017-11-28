f92cd8ff27c37370cf49af6326afe02d450ed502
agoetc <b6151@oic.jp>
2017-11-28 14:21:10 +0900

    register titleAdd file created

---
+++ OicRentalShop/OicRentalShop/Register/Title/TitleAdd.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace OicRentalShop.Register.ItemAdd
+{
+    partial class ItemAdd
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Add = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.dgv_ItemRe = new System.Windows.Forms.DataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.lbl_ReleaseDate = new System.Windows.Forms.Label();
+            this.lbl_Genre = new System.Windows.Forms.Label();
+            this.lbl_Artist = new System.Windows.Forms.Label();
+            this.lbl_Title = new System.Windows.Forms.Label();
+            this.lbl_UniqueID = new System.Windows.Forms.Label();
+            this.lbl_TopTitle = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ItemRe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Add
+            // 
+            this.btn_Add.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_Add.Location = new System.Drawing.Point(788, 509);
+            this.btn_Add.Name = "btn_Add";
+            this.btn_Add.Size = new System.Drawing.Size(97, 70);
+            this.btn_Add.TabIndex = 121;
+            this.btn_Add.Text = "登録";
+            this.btn_Add.UseVisualStyleBackColor = true;
+            this.btn_Add.Click += new System.EventHandler(this.btn_Add_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("MS UI Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btn_clear.Location = new System.Drawing.Point(648, 509);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(97, 70);
+            this.btn_clear.TabIndex = 120;
+            this.btn_clear.Text = "クリア";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            // 
+            // dgv_ItemRe
+            // 
+            this.dgv_ItemRe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_ItemRe.Location = new System.Drawing.Point(630, 123);
+            this.dgv_ItemRe.Name = "dgv_ItemRe";
+            this.dgv_ItemRe.RowTemplate.Height = 21;
+            this.dgv_ItemRe.Size = new System.Drawing.Size(288, 362);
+            this.dgv_ItemRe.TabIndex = 119;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(223, 464);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(184, 21);
+            this.label7.TabIndex = 118;
+            this.label7.Text = "19XX年XX月XX日";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(223, 412);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 21);
+            this.label8.TabIndex = 117;
+            this.label8.Text = "アダルト";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label10.Location = new System.Drawing.Point(223, 360);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(98, 21);
+            this.label10.TabIndex = 116;
+            this.label10.Text = "田所浩二";
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label12.Location = new System.Drawing.Point(223, 270);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(382, 45);
+            this.label12.TabIndex = 115;
+            this.label12.Text = "真夏の夜の淫夢　昏睡レイプ　野獣と化かした先輩";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label13.Location = new System.Drawing.Point(223, 218);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(118, 21);
+            this.label13.TabIndex = 114;
+            this.label13.Text = "666666666";
+            // 
+            // lbl_ReleaseDate
+            // 
+            this.lbl_ReleaseDate.AutoSize = true;
+            this.lbl_ReleaseDate.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_ReleaseDate.Location = new System.Drawing.Point(83, 464);
+            this.lbl_ReleaseDate.Name = "lbl_ReleaseDate";
+            this.lbl_ReleaseDate.Size = new System.Drawing.Size(88, 21);
+            this.lbl_ReleaseDate.TabIndex = 113;
+            this.lbl_ReleaseDate.Text = "発売日：";
+            // 
+            // lbl_Genre
+            // 
+            this.lbl_Genre.AutoSize = true;
+            this.lbl_Genre.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_Genre.Location = new System.Drawing.Point(59, 412);
+            this.lbl_Genre.Name = "lbl_Genre";
+            this.lbl_Genre.Size = new System.Drawing.Size(112, 21);
+            this.lbl_Genre.TabIndex = 112;
+            this.lbl_Genre.Text = "ジャンル名：";
+            // 
+            // lbl_Artist
+            // 
+            this.lbl_Artist.AutoSize = true;
+            this.lbl_Artist.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_Artist.Location = new System.Drawing.Point(29, 360);
+            this.lbl_Artist.Name = "lbl_Artist";
+            this.lbl_Artist.Size = new System.Drawing.Size(142, 21);
+            this.lbl_Artist.TabIndex = 111;
+            this.lbl_Artist.Text = "アーティスト名：";
+            // 
+            // lbl_Title
+            // 
+            this.lbl_Title.AutoSize = true;
+            this.lbl_Title.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_Title.Location = new System.Drawing.Point(62, 270);
+            this.lbl_Title.Name = "lbl_Title";
+            this.lbl_Title.Size = new System.Drawing.Size(109, 21);
+            this.lbl_Title.TabIndex = 110;
+            this.lbl_Title.Text = "タイトルID：";
+            // 
+            // lbl_UniqueID
+            // 
+            this.lbl_UniqueID.AutoSize = true;
+            this.lbl_UniqueID.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_UniqueID.Location = new System.Drawing.Point(84, 218);
+            this.lbl_UniqueID.Name = "lbl_UniqueID";
+            this.lbl_UniqueID.Size = new System.Drawing.Size(87, 21);
+            this.lbl_UniqueID.TabIndex = 109;
+            this.lbl_UniqueID.Text = "固有ID：";
+            // 
+            // lbl_TopTitle
+            // 
+            this.lbl_TopTitle.AutoSize = true;
+            this.lbl_TopTitle.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lbl_TopTitle.Location = new System.Drawing.Point(252, 11);
+            this.lbl_TopTitle.Name = "lbl_TopTitle";
+            this.lbl_TopTitle.Size = new System.Drawing.Size(288, 64);
+            this.lbl_TopTitle.TabIndex = 108;
+            this.lbl_TopTitle.Text = "商品登録";
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label9.Location = new System.Drawing.Point(-1, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(950, 1);
+            this.label9.TabIndex = 107;
+            // 
+            // ItemAdd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_Add);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.dgv_ItemRe);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.lbl_ReleaseDate);
+            this.Controls.Add(this.lbl_Genre);
+            this.Controls.Add(this.lbl_Artist);
+            this.Controls.Add(this.lbl_Title);
+            this.Controls.Add(this.lbl_UniqueID);
+            this.Controls.Add(this.lbl_TopTitle);
+            this.Controls.Add(this.label9);
+            this.Name = "ItemAdd";
+            this.Size = new System.Drawing.Size(948, 591);
+            this.Load += new System.EventHandler(this.ItemAdd_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_ItemRe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Add;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.DataGridView dgv_ItemRe;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label lbl_ReleaseDate;
+        private System.Windows.Forms.Label lbl_Genre;
+        private System.Windows.Forms.Label lbl_Artist;
+        private System.Windows.Forms.Label lbl_Title;
+        private System.Windows.Forms.Label lbl_UniqueID;
+        private System.Windows.Forms.Label lbl_TopTitle;
+        private System.Windows.Forms.Label label9;
+    }
+}
