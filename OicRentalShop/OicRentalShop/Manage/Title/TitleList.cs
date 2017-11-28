4c9c32d58c717515889cae3ef8e67893423640a7
unknown <b6151@oic.jp>
2017-11-28 16:19:45 +0900

    一時こみっと

---
+++ OicRentalShop/OicRentalShop/Manage/Title/TitleList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OicRentalShop.Manage.Title
+{
+    public partial class TitleList : Form
+    {
+        public TitleList()
+        {
+            InitializeComponent();
+        }
+
+        private void TitleList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
