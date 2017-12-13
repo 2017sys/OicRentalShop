1deb9193464fc189adb22f9d8fea1421bdc5ab00
unknown <B6081@oic.osaka.jp>
2017-12-13 14:21:05 +0900

    イベント追加にOLDを追加

--- OicRentalShop/OicRentalShop/Manage/Event/EventAdd.Designer.cs
+++ OicRentalShop/OicRentalShop/Manage/Event/EventAdd.Designer.cs
@@ -30,14 +30,12 @@
         {
             this.label4 = new System.Windows.Forms.Label();
             this.lbl_TopTitle = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.txt_EventID = new System.Windows.Forms.TextBox();
             this.txt_EventName = new System.Windows.Forms.TextBox();
             this.txt_EventPrice = new System.Windows.Forms.TextBox();
             this.txt_ItemType = new System.Windows.Forms.TextBox();
@@ -46,6 +44,8 @@
             this.date_End = new System.Windows.Forms.DateTimePicker();
             this.btn_Add = new System.Windows.Forms.Button();
             this.btn_Redo = new System.Windows.Forms.Button();
+            this.lbl_Old = new System.Windows.Forms.Label();
+            this.cmb_Old = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label4
@@ -67,21 +67,11 @@
             this.lbl_TopTitle.TabIndex = 5;
             this.lbl_TopTitle.Text = "イベント管理";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(251, 132);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 27);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "イベントID";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(251, 188);
+            this.label2.Location = new System.Drawing.Point(251, 102);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(125, 27);
             this.label2.TabIndex = 7;
@@ -91,7 +81,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(251, 241);
+            this.label3.Location = new System.Drawing.Point(251, 154);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(68, 27);
             this.label3.TabIndex = 8;
@@ -101,7 +91,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(251, 289);
+            this.label5.Location = new System.Drawing.Point(251, 206);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(130, 27);
             this.label5.TabIndex = 9;
@@ -111,7 +101,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(251, 336);
+            this.label6.Location = new System.Drawing.Point(251, 311);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(68, 27);
             this.label6.TabIndex = 10;
@@ -121,7 +111,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label7.Location = new System.Drawing.Point(251, 391);
+            this.label7.Location = new System.Drawing.Point(251, 363);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(96, 27);
             this.label7.TabIndex = 11;
@@ -131,57 +121,50 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
-            this.label8.Location = new System.Drawing.Point(251, 443);
+            this.label8.Location = new System.Drawing.Point(251, 415);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(96, 27);
             this.label8.TabIndex = 12;
             this.label8.Text = "終了日";
             // 
-            // txt_EventID
-            // 
-            this.txt_EventID.Location = new System.Drawing.Point(446, 140);
-            this.txt_EventID.Name = "txt_EventID";
-            this.txt_EventID.Size = new System.Drawing.Size(211, 19);
-            this.txt_EventID.TabIndex = 13;
-            // 
             // txt_EventName
             // 
-            this.txt_EventName.Location = new System.Drawing.Point(446, 196);
+            this.txt_EventName.Location = new System.Drawing.Point(455, 110);
             this.txt_EventName.Name = "txt_EventName";
             this.txt_EventName.Size = new System.Drawing.Size(211, 19);
             this.txt_EventName.TabIndex = 14;
             // 
             // txt_EventPrice
             // 
-            this.txt_EventPrice.Location = new System.Drawing.Point(446, 249);
+            this.txt_EventPrice.Location = new System.Drawing.Point(455, 162);
             this.txt_EventPrice.Name = "txt_EventPrice";
             this.txt_EventPrice.Size = new System.Drawing.Size(211, 19);
             this.txt_EventPrice.TabIndex = 15;
             // 
             // txt_ItemType
             // 
-            this.txt_ItemType.Location = new System.Drawing.Point(446, 297);
+            this.txt_ItemType.Location = new System.Drawing.Point(455, 214);
             this.txt_ItemType.Name = "txt_ItemType";
             this.txt_ItemType.Size = new System.Drawing.Size(211, 19);
             this.txt_ItemType.TabIndex = 16;
             // 
             // txt_ItemNum
             // 
-            this.txt_ItemNum.Location = new System.Drawing.Point(446, 346);
+            this.txt_ItemNum.Location = new System.Drawing.Point(455, 319);
             this.txt_ItemNum.Name = "txt_ItemNum";
             this.txt_ItemNum.Size = new System.Drawing.Size(211, 19);
             this.txt_ItemNum.TabIndex = 17;
             // 
             // date_Start
             // 
-            this.date_Start.Location = new System.Drawing.Point(446, 399);
+            this.date_Start.Location = new System.Drawing.Point(455, 371);
             this.date_Start.Name = "date_Start";
             this.date_Start.Size = new System.Drawing.Size(211, 19);
             this.date_Start.TabIndex = 20;
             // 
             // date_End
             // 
-            this.date_End.Location = new System.Drawing.Point(446, 451);
+            this.date_End.Location = new System.Drawing.Point(455, 423);
             this.date_End.Name = "date_End";
             this.date_End.Size = new System.Drawing.Size(211, 19);
             this.date_End.TabIndex = 21;
@@ -208,10 +191,35 @@
             this.btn_Redo.UseVisualStyleBackColor = true;
             this.btn_Redo.Click += new System.EventHandler(this.btn_Redo_Click);
             // 
+            // lbl_Old
+            // 
+            this.lbl_Old.AutoSize = true;
+            this.lbl_Old.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Bold);
+            this.lbl_Old.Location = new System.Drawing.Point(251, 258);
+            this.lbl_Old.Name = "lbl_Old";
+            this.lbl_Old.Size = new System.Drawing.Size(139, 27);
+            this.lbl_Old.TabIndex = 24;
+            this.lbl_Old.Text = "旧作/新作";
+            // 
+            // cmb_Old
+            // 
+            this.cmb_Old.FormattingEnabled = true;
+            this.cmb_Old.Items.AddRange(new object[] {
+            "すべて",
+            "旧作",
+            "新作"});
+            this.cmb_Old.Location = new System.Drawing.Point(455, 266);
+            this.cmb_Old.Name = "cmb_Old";
+            this.cmb_Old.Size = new System.Drawing.Size(211, 20);
+            this.cmb_Old.TabIndex = 25;
+            this.cmb_Old.Text = "すべて";
+            // 
             // EventAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cmb_Old);
+            this.Controls.Add(this.lbl_Old);
             this.Controls.Add(this.btn_Redo);
             this.Controls.Add(this.btn_Add);
             this.Controls.Add(this.date_End);
@@ -220,14 +228,12 @@
             this.Controls.Add(this.txt_ItemType);
             this.Controls.Add(this.txt_EventPrice);
             this.Controls.Add(this.txt_EventName);
-            this.Controls.Add(this.txt_EventID);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbl_TopTitle);
             this.Controls.Add(this.label4);
             this.Name = "EventAdd";
@@ -242,14 +248,12 @@
 
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label lbl_TopTitle;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.TextBox txt_EventID;
         private System.Windows.Forms.TextBox txt_EventName;
         private System.Windows.Forms.TextBox txt_EventPrice;
         private System.Windows.Forms.TextBox txt_ItemType;
@@ -258,5 +262,7 @@
         private System.Windows.Forms.DateTimePicker date_End;
         private System.Windows.Forms.Button btn_Add;
         private System.Windows.Forms.Button btn_Redo;
+        private System.Windows.Forms.Label lbl_Old;
+        private System.Windows.Forms.ComboBox cmb_Old;
     }
 }
--- OicRentalShop/OicRentalShop/Manage/Event/EventAdd.cs
+++ OicRentalShop/OicRentalShop/Manage/Event/EventAdd.cs
@@ -35,10 +35,26 @@ namespace OicRentalShop.Manage.Event
 
         private void btn_Add_Click(object sender, EventArgs e)
         {
+
+
+            string old="null";
+            if(cmb_Old.Text=="旧作")
+            {
+                old = "1";
+            }
+            else if(cmb_Old.Text=="新作")
+            {
+                old = "2";
+            }
+            else if(cmb_Old.Text=="すべて")
+            {
+
+            }
+            
             string sd = date_Start.Value.ToString("yyyy/MM/dd");
             string ed = date_End.Value.ToString("yyyy/MM/dd");
             MessageBox.Show(sd);
-            CmdFunc("INSERT INTO TBL_EVENT(EVENT_NAME,EVENT_PRICE,TYPE_ID,EVENT_NUM,EVENT_START,EVENT_END,EVENT_OLD,EVENT_DELETE)  VALUES('"+txt_EventName.Text+"',"+txt_EventPrice.Text+","+txt_ItemType.Text+","+txt_ItemNum.Text+",#"+sd+"#,#"+ed+"#,1,false)");
+            CmdFunc("INSERT INTO TBL_EVENT(EVENT_NAME,EVENT_PRICE,TYPE_ID,EVENT_NUM,EVENT_START,EVENT_END,EVENT_OLD,EVENT_DELETE)  VALUES('"+txt_EventName.Text+"',"+txt_EventPrice.Text+","+txt_ItemType.Text+","+txt_ItemNum.Text+",#"+sd+"#,#"+ed+"#,"+old+",false)");
         }
 
         private void CmdFunc(string cmdstr) //UPDATE DELETE INSERTを実行できる　CmdFunc("SQL文"); 
