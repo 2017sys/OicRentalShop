267a8b03e28ce2473630d5b2123eed3cd7d9cf7d
unknown <B6081@oic.osaka.jp>
2018-01-10 13:42:44 +0900

    PriceListのdatagridviewのプロパティを変更 Please enter the commit message for your changes. Lines starting

--- OicRentalShop/OicRentalShop/Manage/Price/PriceList.Designer.cs
+++ OicRentalShop/OicRentalShop/Manage/Price/PriceList.Designer.cs
@@ -41,39 +41,36 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("MS UI Gothic", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label4.Location = new System.Drawing.Point(484, 6);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(363, 5);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(277, 80);
+            this.label4.Size = new System.Drawing.Size(223, 64);
             this.label4.TabIndex = 220;
             this.label4.Text = "料金表";
             // 
             // label9
             // 
             this.label9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label9.Location = new System.Drawing.Point(-1, 99);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Location = new System.Drawing.Point(-1, 79);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(1267, 1);
+            this.label9.Size = new System.Drawing.Size(950, 1);
             this.label9.TabIndex = 211;
             // 
             // dgv_PriceInfo
             // 
+            this.dgv_PriceInfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgv_PriceInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_PriceInfo.Location = new System.Drawing.Point(104, 135);
-            this.dgv_PriceInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgv_PriceInfo.Location = new System.Drawing.Point(78, 108);
             this.dgv_PriceInfo.Name = "dgv_PriceInfo";
             this.dgv_PriceInfo.RowTemplate.Height = 21;
-            this.dgv_PriceInfo.Size = new System.Drawing.Size(932, 544);
+            this.dgv_PriceInfo.Size = new System.Drawing.Size(699, 435);
             this.dgv_PriceInfo.TabIndex = 221;
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(1063, 402);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(797, 322);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(160, 88);
+            this.button1.Size = new System.Drawing.Size(120, 70);
             this.button1.TabIndex = 222;
             this.button1.Text = "追加";
             this.button1.UseVisualStyleBackColor = true;
@@ -81,10 +78,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button2.Location = new System.Drawing.Point(1063, 498);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(797, 398);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(160, 88);
+            this.button2.Size = new System.Drawing.Size(120, 70);
             this.button2.TabIndex = 223;
             this.button2.Text = "編集";
             this.button2.UseVisualStyleBackColor = true;
@@ -92,17 +88,16 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button3.Location = new System.Drawing.Point(1063, 592);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(797, 474);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(160, 88);
+            this.button3.Size = new System.Drawing.Size(120, 70);
             this.button3.TabIndex = 224;
             this.button3.Text = "決定";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // PriceList
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -110,9 +105,8 @@
             this.Controls.Add(this.dgv_PriceInfo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label9);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PriceList";
-            this.Size = new System.Drawing.Size(1264, 739);
+            this.Size = new System.Drawing.Size(948, 591);
             this.Load += new System.EventHandler(this.PriceList_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgv_PriceInfo)).EndInit();
             this.ResumeLayout(false);
